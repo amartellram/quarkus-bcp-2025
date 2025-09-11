5e9b645f8386abd85646b4713c2cc07a21b278d2
Abel Martell <amartellram@gmail.com>
2025-09-11 11:31:29 -0500

    complete lab14

--- 12-secure-sso-start/expense/get_token.sh
+++ 12-secure-sso-start/expense/get_token.sh
@@ -11,17 +11,22 @@ fi
 SERVER="http://localhost:8888/realms/quarkus/protocol/openid-connect/token"
 SECRET_ID="backend-service"
 SECRET_PW="secret"
-USERNAME="$1"
-PASSWORD="$2"
+USERNAME="user"
+PASSWORD="redhat"
 
-export TOKEN=$(curl --insecure -s -X POST "$SERVER" \
-  --user ${SECRET_ID}:${SECRET_PW} \
+RESPONSE=$(curl --insecure -s -X POST "$SERVER" \
   -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "client_id=${SECRET_ID}" \
+  -d "client_secret=${SECRET_PW}" \
   -d "username=${USERNAME}" \
   -d "password=${PASSWORD}" \
-  -d 'grant_type=password' \
-  | jq --raw-output '.access_token'
-)
+  -d 'grant_type=password')
+
+echo "Respuesta completa de Keycloak:"
+echo "$RESPONSE"
+
+export TOKEN=$(echo "$RESPONSE" | jq --raw-output '.access_token')
+
 
 if [[ "$TOKEN" == "null" ]] || [[ "$TOKEN" == ""  ]]; then
     echo 1>&2 "Token was not retrieved! Review input parameters."
